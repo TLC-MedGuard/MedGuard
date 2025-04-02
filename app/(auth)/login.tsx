638dfba134ee6fce490dc150b385340d1a5c4ea5
ChelseaHagosojos <chelseahagosojos@thelewiscollege.edu.ph>
2025-04-02 22:53:03 +0800

    Test push for dc bot webhook

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -5,7 +5,7 @@ import { styles } from '@/styles/auth.styles'
 export default function login() {
   return (
     <View style={styles.container}>
-      <Text>login</Text>
+      <Text>Login Page</Text>
     </View>
   )
 }
